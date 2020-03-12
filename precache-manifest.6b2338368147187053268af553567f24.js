4fc15c8731dc5be0abe572445b7e21e4ba06f5d3
Piotr Lasa <p.lasa@outlook.com>
2020-03-12 17:00:53 +0100

    Updates

---
+++ precache-manifest.6b2338368147187053268af553567f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0821ac9235d6ac45c078181856f3359e",
+    "url": "/Bookself-app/index.html"
+  },
+  {
+    "revision": "ccbda9179ab08e5c05ab",
+    "url": "/Bookself-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "368568010868c4196e2a",
+    "url": "/Bookself-app/static/css/main.9a390dc3.chunk.css"
+  },
+  {
+    "revision": "ccbda9179ab08e5c05ab",
+    "url": "/Bookself-app/static/js/2.f103947f.chunk.js"
+  },
+  {
+    "revision": "cd6412064f2eb0301b774fcc1614e06f",
+    "url": "/Bookself-app/static/js/2.f103947f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368568010868c4196e2a",
+    "url": "/Bookself-app/static/js/main.b95d0238.chunk.js"
+  },
+  {
+    "revision": "9064dbde0f98d1a5c84c",
+    "url": "/Bookself-app/static/js/runtime-main.6180b29c.js"
+  }
+]);
